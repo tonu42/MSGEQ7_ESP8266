0023ef33f5bcc1ee39966d7834e6a860730930c0
tonu42 <tonu42@gmail.com>
2016-08-11 21:09:30 -0400

    Reduce Jitter
    Previous code produced JSON either too fast, or too slow, can't really
    tell. Either way it was stuttering the bars. Now the ESP will send a
    "passcode" for the server to acknowldge it is the only source of data,
    and does not send it back. Perhaps saving ESP cpu clocks. And making
    graphs MUCH smoother.

--- JAVA/src/java/Endpoint/Endpoint.java
+++ JAVA/src/java/Endpoint/Endpoint.java
@@ -23,18 +23,25 @@ import javax.websocket.server.ServerEndpoint;
 public class Endpoint {
 
     private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
-    
+    private Session espcon;
     @OnOpen
     public void onOpen(Session session){
         sessions.add(session);
+        
         System.out.println("New Client: " + session.getId() + " Connected.");
     }
     
     @OnMessage
-    public String onMessage(String message) {
-       // System.out.println(message);
+    public String onMessage(Session session, String message) {
+       //
+       if(message.equals("ESPCON"))
+       System.out.println("ESP_MASTER CONNECTED");
+       if(message.equals("ESPCON")){
+           espcon = session;
+       }
         for(Session s : sessions){
             try {
+                if(s != espcon)
                 s.getBasicRemote().sendText(message);
             } catch (IOException ex) {
                 ex.printStackTrace();
